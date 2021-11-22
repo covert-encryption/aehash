b7385748dc43c0e78e4f13a2769dd5bcf62ef6be
foonoxous <RWQe4qDs7LIsmI7Q94ctNMSfwfDW8eld24YGevEzQEKZyfJz1NPlH6uf>
2021-11-22 14:44:45 +0000

    Add a ChaCha20-Poly1305 variant for comparison (it has very similar performance on PC despite having no hardware acceleration).

---
+++ aehash-chacha20-poly1305.py
@@ -0,0 +1,49 @@
+"""A variant of AEhash based on ChaCha20-Poly1305 as a drop-in replacement for AES256-GCM."""
+# pip install pynacl
+from hashlib import sha512
+from sys import argv, exit, stderr
+from unicodedata import normalize
+
+from nacl.bindings import crypto_aead_chacha20poly1305_ietf_encrypt as chapoly
+
+
+def aehash(password: bytes, salt: bytes, mem=500, ops=10) -> bytes:
+    """Derive a 32-byte hash for (password, salt) using mem MiB and ops iterations."""
+    if mem <= 0 or ops < 2 or mem * ops > 100000:
+        raise ValueError(f"Invalid cost parameters {mem=} {ops=}")
+    nonce = sha(salt)[:12]
+    key = sha(password)[:32]
+    size = mem << 20  # MiB
+    buf = bytes(size)  # Initially all zeroes
+    for i in range(ops):
+        # ChaCha20-Poly1305, returns size bytes ciphertext + 16 bytes GCM tag
+        buf = chapoly(buf[:size], None, nonce, key)
+        key = buf[-32:]  # 16 encrypted bytes + 16 bytes gcm tag
+    # Hash the final key for output
+    return sha(key)[:32]
+
+
+def sha(data: bytes) -> bytes:
+    return sha512(data).digest()
+
+def encode(s: str) -> bytes:
+    """Encode text into bytes with Unicode normalization."""
+    return normalize("NFKC", s).encode()
+
+def main():
+    try:
+        if len(argv) != 5:
+            raise ValueError(
+                f"Usage: {argv[0]} PASSWORD SALT MEM OPS\n\n"
+                "  MEM is in megabytes, OPS is the number of iterations.\n"
+                "  The duration is relative to MEM * OPS."
+            )
+        pw, salt = encode(argv[1]), encode(argv[2])
+        mem, ops = int(argv[3]), int(argv[4])
+        print(aehash(pw, salt, mem, ops).hex())
+    except ValueError as e:
+        stderr.write(f"{e}\n")
+        return 1
+
+if __name__ == "__main__":
+    exit(main() or 0)
